81d5ca2c89a719856511ed37d94c22821062aa56
pedrohff <pedrohenriqueff@gmail.com>
2016-05-30 18:08:30 -0300

    v1.0.0

---
+++ portraits.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+            $("li a.tracer").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/itqzr89.png\"'></div></div>");
+            });
+            $("li a.widowmaker").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/tcVfItX.png\"'></div></div>");
+            });
+            $("li a.dva").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/JvZX2Tz.png\"'></div></div>");
+            });
+            $("li a.bastion").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/RNuoZUc.png\"'></div></div>");
+            });
+            $("li a.zenyatta").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/7c6kjsq.png\"'></div></div>");
+            });
+            $("li a.zarya").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/goPmTJR.png\"'></div></div>");
+            });
+            $("li a.winston").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/zxsk3K7.png\"'></div></div>");
+            });
+            $("li a.torbjorn").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/KmAM59H.png\"'></div></div>");
+            });
+            $("li a.symmetra").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/AC11et9.png\"'></div></div>");
+            });
+            $("li a.soldier76").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/zo8ht6D.png\"'></div></div>");
+            });
+            $("li a.roadhog").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/D5025DQ.png\"'></div></div>");
+            });
+            $("li a.reinhardt").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/vtHeQ6q.png\"'></div></div>");
+            });
+            $("li a.reaper").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/XFiVCr4.png\"'></div></div>");
+            });
+            $("li a.pharah").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/hm7uJj0.png\"'></div></div>");
+            });
+            $("li a.mercy").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/1Ggx1bD.png\"'></div></div>");
+            });
+            $("li a.mei").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/jXVCeaK.png\"'></div></div>");
+            });
+            $("li a.mccree").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/ELhxfoM.png\"'></div></div>");
+            });
+            $("li a.lucio").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/9hAoP5K.png\"'></div></div>");
+            });
+            $("li a.junkrat").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/dz1CoIc.png\"'></div></div>");
+            });
+            $("li a.hanzo").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/zPxR6N9.png\"'></div></div>");
+            });
+            $("li a.genji").each(function(){
+                $(this).html("<div class='tagimg'><div style='background-image:url(\"http://i.imgur.com/e8JQtZY.png\"'></div></div>");
+            });
+
+        });
